0c163d377b714189e358201b84993d0673369ff4
Miguel Salinas Gancedo <masalinas.gancedo@gmail.com>
2024-11-12 19:06:00 +0100

    first commit

---
+++ src/main/java/es/uniovi/edv/avispe/uniovi_avib_morphingprojections_gateway/UnioviAvibMorphingprojectionsGatewayApplication.java
@@ -0,0 +1,13 @@
+package es.uniovi.edv.avispe.uniovi_avib_morphingprojections_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnioviAvibMorphingprojectionsGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnioviAvibMorphingprojectionsGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/uniovi/edv/avispe/uniovi_avib_morphingprojections_gateway/UnioviAvibMorphingprojectionsGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package es.uniovi.edv.avispe.uniovi_avib_morphingprojections_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnioviAvibMorphingprojectionsGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
